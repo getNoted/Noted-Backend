5466538d891f8b9fdabefe3f9f7d4e780829bd9a
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-03-18 06:15:09 +0530

    added the api route

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+
+// api route
+const api = require("./routes/api");
 const app = express();
+app.use("/api", api);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
