1dc15305be330525d865c43020ff5cdcecde1c3c
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-03-18 06:21:01 +0530

    added the signin and signup routes

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,4 +2,6 @@ const { Router } = require("express");
 const express = require("express");
 const router = express.Router();
 
+// routes
+const signup=require('./signup');
 module.exports = router;
