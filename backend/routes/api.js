774824fc274b3f4ef6e07ff257e7d25a4706f403
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-03-20 19:24:30 +0530

    implemeted signup route

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,8 @@
-const { Router } = require("express");
+const { Router, application } = require("express");
 const express = require("express");
 const router = express.Router();
 
 // routes
 const signup=require('./signup');
-
+router.use('/signup',signup);
 module.exports = router;
