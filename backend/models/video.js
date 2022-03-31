b4e4c5413841c4916c89e999b469dcb9faa776ba
RavjotSandhu <nand2072@gmail.com>
2022-03-31 08:34:03 +0530

    create video schema

---
+++ backend/models/video.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const videotimestampSchema = new mongoose.Schema({
+  note:{
+    type: String,
+    required: true,
+  }
+  timestamp:{
+    type: String,
+    required: true,
+  }
+})
+const videoSchema = new mongoose.Schema({
+  video_id: {
+    type: String,
+    required: true,
+  },
+  video_name: {
+    type: String,
+    required: true,
+  },
+  video_url: {
+    type: String,
+    required: true,
+  },
+  folder:{
+    type: String,
+    required: true,
+  },
+  notes:
+    [videotimestampSchema]
+}, {timestamps: { createdAt: 'created_at' },{updatedAt: 'modified_on'}});
+
+const Video = mongoose.model("Video", videoSchema);
+const VideoTimestamp = mongoose.model("VideoTimestamp", videotimestampSchema);
+module.exports = Video;
+module.exports = VideoTimestamp;
