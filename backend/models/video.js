eb9e425b89facb74121ab94baa611142f15fa33f
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-04-01 12:51:41 +0530

    edited video.js file

--- backend/models/video.js
+++ backend/models/video.js
@@ -1,36 +1,33 @@
 const mongoose = require("mongoose");
-const videotimestampSchema = new mongoose.Schema({
-  note:{
-    type: String,
-    required: true,
+
+const videoSchema = new mongoose.Schema(
+  {
+    video_id: {
+      type: String,
+      required: true,
+    },
+    user_id: mongoose.Schema.Types.ObjectId,
+    video_name: {
+      type: String,
+      required: true,
+    },
+    video_url: {
+      type: String,
+      required: true,
+    },
+    folder: {
+      type: String,
+      required: true,
+    },
+    notes: {
+      type: Map,
+      of: String,
+      required: true,
+    },
   },
-  timestamp:{
-    type: String,
-    required: true,
-  }
-})
-const videoSchema = new mongoose.Schema({
-  video_id: {
-    type: String,
-    required: true,
-  },
-  video_name: {
-    type: String,
-    required: true,
-  },
-  video_url: {
-    type: String,
-    required: true,
-  },
-  folder:{
-    type: String,
-    required: true,
-  },
-  notes:
-    [videotimestampSchema]
-}, {timestamps: { createdAt: 'created_at',updatedAt: 'modified_on'}});
+  { timestamps: { createdAt: "created_at", updatedAt: "modified_on" } }
+);
 
 const Video = mongoose.model("Video", videoSchema);
-const VideoTimestamp = mongoose.model("VideoTimestamp", videotimestampSchema);
+
 module.exports = Video;
-module.exports = VideoTimestamp;
