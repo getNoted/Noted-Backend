09c86fe6a546ceecd113e1abd5beee2dadef9425
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-04-02 12:29:53 +0530

    resolving conflicts

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -7,15 +7,11 @@ const router = express.Router();
 const signup = require("./signup");
 const signin = require("./signin");
 const changepassword = require("./changepassword");
-const createnotes=require('./createnotes');
-const readtimestampnotes=require('./timestampnotes')
+const createnotes = require("./createnotes");
+const readtimestampnotes = require("./timestampnotes");
 
 router.use("/v1/signup", signup);
 router.use("/v1/signin", signin);
 router.use("/v1/resetpassword", changepassword);
-
-<<<<<<< HEAD
-router.use('/v1/notes/timestamp/read',readtimestampnotes);
-=======
->>>>>>> 59ba6c6f7964ed91d97dacba94761a72b055ac80
+router.use("/v1/notes/timestamp/read", readtimestampnotes);
 module.exports = router;
