040f05441a258b4030062b8f250e167f1e4cfc3c
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-04-02 09:10:42 +0530

    applied prettier

--- backend/controllers/notes/createnotes.js
+++ backend/controllers/notes/createnotes.js
@@ -1,67 +1,70 @@
-const jwt=require('jsonwebtoken');
-const Video =require('../../models/video');
-const url=require('url');
-const formattimestamp=(timestamp)=>{
+const jwt = require("jsonwebtoken");
+const Video = require("../../models/video");
+const url = require("url");
+const formattimestamp = (timestamp) => {
   const timearr = timestamp.split(":");
   while (timearr.length !== 3) {
     timearr.unshift("00");
   }
-  let formattedtimestamp="";
-  timearr.forEach((element,index) => {
-      if(index===2) formattedtimestamp+=element;
-      else
-          formattedtimestamp+=element+":";
+  let formattedtimestamp = "";
+  timearr.forEach((element, index) => {
+    if (index === 2) formattedtimestamp += element;
+    else formattedtimestamp += element + ":";
   });
   console.log(formattedtimestamp);
   return formattedtimestamp;
-}
+};
 const createnotes = async (req, res) => {
   const token = req.headers["authorization"].split(" ")[1];
- 
-  try{
-    const user=jwt.verify(token,process.env.JWT_AUTH_SECRET);
-    if(!user){
-      res.status(404).json({message:"user not logged in"})
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_AUTH_SECRET);
+    if (!user) {
+      res.status(404).json({ message: "user not logged in" });
     }
-    const user_id=user._id;
-    let {video_url,videoname:video_name,timestamp,content,foldername:folder}=req.body;
+    const user_id = user._id;
+    let {
+      video_url,
+      videoname: video_name,
+      timestamp,
+      content,
+      foldername: folder,
+    } = req.body;
     console.log(video_url);
-    const {v:video_id}=url.parse(video_url,true).query;
-    
-    const video=await Video.findOne({video_id, user_id});
-    timestamp=formattimestamp(timestamp);
-    if(video){
-      const updatednotes=video.notes;
-      updatednotes.set(timestamp,content);
-      
-       await Video.updateOne({user_id,video_id},{
-         $set:{
-           notes:updatednotes
-         }
-       })
-       res.status(200).json({message:"success"});
-    }
-    else{
-      const notes=new Map();
-      notes.set(timestamp,content);
-      const newVideo=await Video.create({
+    const { v: video_id } = url.parse(video_url, true).query;
+
+    const video = await Video.findOne({ video_id, user_id });
+    timestamp = formattimestamp(timestamp);
+    if (video) {
+      const updatednotes = video.notes;
+      updatednotes.set(timestamp, content);
+
+      await Video.updateOne(
+        { user_id, video_id },
+        {
+          $set: {
+            notes: updatednotes,
+          },
+        }
+      );
+      res.status(200).json({ message: "success" });
+    } else {
+      const notes = new Map();
+      notes.set(timestamp, content);
+      const newVideo = await Video.create({
         video_id,
         video_url,
         user_id,
         video_name,
         folder,
-        notes
+        notes,
       });
 
-      res.status(200).json({message:"success"})
+      res.status(200).json({ message: "success" });
     }
-    
-  }catch(err){
+  } catch (err) {
     console.log(err);
   }
- 
 };
 
-
-
-module.exports={createnotes};
+module.exports = { createnotes };
