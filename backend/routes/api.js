15f8e551a5c3f10438f3d883e5ce2dd5d941e3bf
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-04-03 14:39:32 +0530

    added the route and changed request body

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -9,11 +9,12 @@ const signin = require("./signin");
 const changepassword = require("./changepassword");
 const createnotes = require("./createnotes");
 const readtimestampnotes = require("./timestampnotes");
-
+const deletetimestamp=require("./deletetimestampnotes");
 
 router.use("/v1/signup", signup);
 router.use("/v1/signin", signin);
 router.use("/v1/resetpassword", changepassword);
 router.use("/v1/notes/timestamp/create",createnotes);
 router.use("/v1/notes/timestamp/read", readtimestampnotes);
+router.use("/v1/notes/timestamp/delete",deletetimestamp);
 module.exports = router;
