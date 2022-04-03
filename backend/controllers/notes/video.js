20e7062cbe3994c6a2bfea134775766ebdfda3dd
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-04-03 16:30:28 +0530

    added the route and implementation

---
+++ backend/controllers/notes/video.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const Video = require("../../models/video");
+
+const readnotes = async (req, res) => {
+  const token = req.headers["authorization"].split(" ")[1];
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_AUTH_SECRET);
+    if (!user) {
+      res.status(404).json({ message: "user not logged in" });
+    }
+    const user_id = user._id;
+
+    const { videoname: video_name } = req.params;
+    const video = await Video.findOne({ video_name, user_id });
+    if (video) {
+      const notes = video.notes;
+      console.log(video.notes);
+      const data = changeNotesFormat(video.notes);
+      console.log(data);
+      res.json({ message: "success", data: data });
+    } else {
+      res.status(200).json({ message: "not found" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+const changeNotesFormat = (notes) => {
+  let data = [];
+  for (let timestamp of notes.keys()) {
+    data.push({
+      [timestamp]: notes.get(timestamp),
+    });
+  }
+  return data;
+};
+
+module.exports = { readnotes };
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -9,11 +9,13 @@ const signin = require("./signin");
 const changepassword = require("./changepassword");
 const createnotes = require("./createnotes");
 const readtimestampnotes = require("./timestampnotes");
-
+const video = require("./video");
 
 router.use("/v1/signup", signup);
 router.use("/v1/signin", signin);
 router.use("/v1/resetpassword", changepassword);
-router.use("/v1/notes/timestamp/create",createnotes);
+router.use("/v1/notes/timestamp/create", createnotes);
 router.use("/v1/notes/timestamp/read", readtimestampnotes);
+router.use("/v1/notes/timestamp/read", readtimestampnotes);
+router.use("/v1/video", video);
 module.exports = router;
