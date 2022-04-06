c6f85f27dcc5a67c79a85e0475acd7f605a668a2
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-04-06 23:17:36 +0530

    changed _id to user_id

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -3,11 +3,14 @@ const Video =require('../models/video');
 
 const authByToken=(req)=>{
     const token = req.headers["authorization"].split(" ")[1];
+   console.log(token);
     const user = jwt.verify(token, process.env.JWT_AUTH_SECRET);
     if (!user) {
       res.status(404).json({ message: "user not logged in" });
     }
-    const user_id = user._id;
+    console.log(user);
+    const user_id = user.user_id;
+    console.log(user_id);
     return user_id;
 }
 
