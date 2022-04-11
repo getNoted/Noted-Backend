1aafff61980dce6d18ddb9261a5df29e984d47d0
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-04-12 01:08:48 +0530

    implemented the api

--- backend/controllers/notes/video.js
+++ backend/controllers/notes/video.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const Video = require("../../models/video");
-
+const { authByToken } = require("../../utils/auth");
+const {changeNotesFormat}=require('../../utils/notes');
 const readnotes = async (req, res) => {
   const token = req.headers["authorization"].split(" ")[1];
 
@@ -28,14 +29,34 @@ const readnotes = async (req, res) => {
     console.log(err);
   }
 };
-const changeNotesFormat = (notes) => {
-  let data = [];
-  for (let timestamp of notes.keys()) {
-    data.push({
-      [timestamp]: notes.get(timestamp),
-    });
-  }
-  return data;
-};
 
-module.exports = { readnotes };
+const editName=async (req,res)=>{
+  const {video_id,new_video_name}=req.body;
+  try{
+    const user_id=authByToken(req);
+    let video=await Video.findOne({user_id,video_name:new_video_name});
+    if(video){
+      console.log(video);
+      res.status(400).json({message:"videoname already exist"});
+    }
+    else{
+      let video=await Video.findOneAndUpdate({user_id,video_id},{
+        $set:{
+          video_name:new_video_name
+        }
+      });
+      console.log(video);
+      if(!video){
+        res.status(404).json({message:"video not found"});
+      }
+      else
+        res.status(200).json({message:"success"});
+    }
+    
+  }catch(err){
+    console.log(err);
+  }
+}
+
+
+module.exports = { readnotes,editName };
--- backend/routes/video.js
+++ backend/routes/video.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { readnotes } = require("../controllers/notes/video");
+const { readnotes, editName } = require("../controllers/notes/video");
 router.get("/:videoname", readnotes);
+router.post("/editname",editName);
 
 module.exports = router;
