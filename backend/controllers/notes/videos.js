e59768fa5d02b4a0d9df236e9d3acd422b8f3ec5
RavjotSandhu <nand2072@gmail.com>
2022-04-15 01:42:38 +0530

    add pagination on videolist

--- backend/controllers/notes/videos.js
+++ backend/controllers/notes/videos.js
@@ -1,17 +1,22 @@
-
 const Video = require("../../models/video");
 const url = require("url");
 const { authByToken } = require("../../utils/auth");
-const readallvideos = async (req,res) => {
 
+const readallvideos = async (req,res) => {
   try {
     const user_id=authByToken(req);
-    const videos = await Video.find({user_id },{ video_id: 1, video_name: 1,video_url:1 });
+    const page=parseInt(req.query.page);
+    const limit=parseInt(req.query.limit);
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const totalDocuments= await Video.countDocuments({user_id });
+
+    const result = await Video.find({user_id },{ video_id: 1, video_name: 1,video_url:1 }).skip(startIndex).limit(limit);
     
-    if (!videos) {
+    if (!result) {
       res.status(404).json({ message: "no video not found" });
     } else {
-      res.status(200).json({ message: "success", videos });
+      res.status(200).json({ message: "success",result });
     }
     
   } catch (err) {
