c92a3a33ff99b8da7348551377a7693ecee8b496
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-04-15 19:01:33 +0530

    used prettier and added comments

--- backend/controllers/folders/folder.js
+++ backend/controllers/folders/folder.js
@@ -6,10 +6,10 @@ const {
   checkIfDeleted,
   softDeleteFolder,
   editName,
-  ifExists
+  ifExists,
 } = require("../../utils/folder");
 
-
+//create a new folder
 const createfolder = async (req, res) => {
   const { folder_name } = req.body;
   console.log(folder_name);
@@ -17,72 +17,60 @@ const createfolder = async (req, res) => {
     const _id = authByToken(req);
     const user = await User.findOne({ _id });
     if (user) {
-      let folder=ifExists(_id,folder_name);
+      let folder = ifExists(_id, folder_name);
       console.log(folder);
-      if (folder.folders.length===0) {
-        folder={folder_name:folder_name,is_deleted:false,user_id:_id};
+      if (folder.folders.length === 0) {
+        folder = { folder_name: folder_name, is_deleted: false, user_id: _id };
         user.folders.push(folder);
         await user.save();
         res.status(200).json({ message: "folder created" });
-        
       } else {
         res.status(400).json({ message: "folder already exists" });
       }
-    }
-    else {
-      res.status(404).json({ message: "user not found" });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-
-
-
-const getFolders=async (req,res)=>{
-  try {
-    const _id = authByToken(req);
-    const user = await User.findOne({ _id });
-    let is_deleted=req.query.deleted;
-    console.log(is_deleted==="true");
-    
-    if (user) {
-     
-
-    const folders = await User.find({ _id },{
-        folders: {
-          $filter: {
-            input: "$folders",
-            as:"folder",
-            cond:{
-              $and:[
-                {$eq:["$$folder.is_deleted",is_deleted==="true"]},
-               
-              ]
-            }
-          },
-         
-        },
-
-        
-        
-      }).sort({'folders.createdAt':1});
-      console.log(folders);
-      res.status(200).json({ message: "success", folders:folders[0].folders});
     } else {
       res.status(404).json({ message: "user not found" });
     }
   } catch (error) {
     console.log(error);
   }
-}
+};
 
+//Get all folders
+const getFolders = async (req, res) => {
+  try {
+    const _id = authByToken(req);
+    const user = await User.findOne({ _id });
+    let is_deleted = req.query.deleted;
+    console.log(is_deleted === "true");
 
+    if (user) {
+      const folders = await User.find(
+        { _id },
+        {
+          folders: {
+            $filter: {
+              input: "$folders",
+              as: "folder",
+              cond: {
+                $and: [{ $eq: ["$$folder.is_deleted", is_deleted === "true"] }],
+              },
+            },
+          },
+        }
+      ).sort({ "folders.createdAt": 1 });
+      console.log(folders);
+      res.status(200).json({ message: "success", folders: folders[0].folders });
+    } else {
+      res.status(404).json({ message: "user not found" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Delete a folder
 const deleteFolder = async (req, res) => {
-  let { folder_id,folder_name } = req.body;
+  let { folder_id, folder_name } = req.body;
   console.log(folder_id);
   folder_id = mongoose.Types.ObjectId(folder_id);
   if (folder_name === "default") {
@@ -90,23 +78,27 @@ const deleteFolder = async (req, res) => {
   } else {
     try {
       const _id = authByToken(req);
-      const folder=await User.findOneAndUpdate({_id:_id,'folders._id':folder_id},{
-        $set:{
-          'folders.$.is_deleted':true
+      const folder = await User.findOneAndUpdate(
+        { _id: _id, "folders._id": folder_id },
+        {
+          $set: {
+            "folders.$.is_deleted": true,
+          },
         }
-      });
+      );
       console.log(folder);
-      
+
       if (folder) {
-        await Video.updateMany({user_id:_id,folder:folder_name},{
-          $set:{
-            is_deleted:true
+        await Video.updateMany(
+          { user_id: _id, folder: folder_name },
+          {
+            $set: {
+              is_deleted: true,
+            },
           }
-        });
-      
-        res.status(200).json({message:"success"});
-        
-        
+        );
+
+        res.status(200).json({ message: "success" });
       } else {
         res.status(404).json({ message: "folder not found" });
       }
@@ -116,49 +108,49 @@ const deleteFolder = async (req, res) => {
   }
 };
 
-
-
 //Edit Name of a folder
 const editFoldername = async (req, res) => {
-  let { new_folder_name, old_folder_name,folder_id } = req.body;
+  let { new_folder_name, old_folder_name, folder_id } = req.body;
   folder_id = mongoose.Types.ObjectId(folder_id);
   if (old_folder_name === "default") {
     res.status(400).json({ message: "default folder cannot be modified" });
   } else {
     try {
       const _id = authByToken(req);
-      
-      let folder=await ifExists(_id,new_folder_name);
-    
-    if(folder.folders.length===0){
-      //rename the folder
-      let folder=await User.findOneAndUpdate({_id:_id,'folders._id':folder_id},{
-        $set:{
-          'folders.$.folder_name':new_folder_name
-        }
-      });
-      
-      if(folder){
-        await Video.updateMany({user_id:_id,folder:old_folder_name},{
-          $set:{
-            folder:new_folder_name
+
+      let folder = await ifExists(_id, new_folder_name);
+
+      if (folder.folders.length === 0) {
+        //rename the folder
+        let folder = await User.findOneAndUpdate(
+          { _id: _id, "folders._id": folder_id },
+          {
+            $set: {
+              "folders.$.folder_name": new_folder_name,
+            },
           }
-        })
-        res.status(200).json({message:"success"});
-      }
-      else{
-        //folder does not exist
-        res.status(404).json({message:"folder does not exist"});
-      }
-    }
+        );
 
-    else{
-      //cannot rename because one with the same name already exists
+        if (folder) {
+          await Video.updateMany(
+            { user_id: _id, folder: old_folder_name },
+            {
+              $set: {
+                folder: new_folder_name,
+              },
+            }
+          );
+          res.status(200).json({ message: "success" });
+        } else {
+          //folder does not exist
+          res.status(404).json({ message: "folder does not exist" });
+        }
+      } else {
+        //cannot rename because one with the same name already exists
 
-      res.status(400).json({message:"already exists"});
-    }
-  
-  } catch (err) {
+        res.status(400).json({ message: "already exists" });
+      }
+    } catch (err) {
       console.log(err);
     }
   }
--- backend/utils/folder.js
+++ backend/utils/folder.js
@@ -1,25 +1,26 @@
-const User =require('../models/user');
+const User = require("../models/user");
 
-const ifExists=async (user_id,folder_name)=>{
-  let folder=await User.findOne({_id:user_id},{
-    folders: {
-      $filter: {
-        input: "$folders",
-        as:"folder",
-        cond:{
-          $and:[
-            {$eq:["$$folder.folder_name",folder_name]},
-            {$eq:["$$folder.is_deleted",false]}
-           
-          ]
-        }
+const ifExists = async (user_id, folder_name) => {
+  let folder = await User.findOne(
+    { _id: user_id },
+    {
+      folders: {
+        $filter: {
+          input: "$folders",
+          as: "folder",
+          cond: {
+            $and: [
+              { $eq: ["$$folder.folder_name", folder_name] },
+              { $eq: ["$$folder.is_deleted", false] },
+            ],
+          },
+        },
       },
-     
     }
-  });
+  );
   console.log(folder);
   return folder;
-}
+};
 
 const softDeleteFolder = (folders, folder_name) => {
   const currentFolder = folders[folder_name];
@@ -32,11 +33,10 @@ const checkIfDeleted = (folders, folder_name) => {
   return currentFolder.is_deleted;
 };
 
-const editName=(folders,old_folder_name,new_folder_name)=>{
-  const currentFolder=folders[old_folder_name];
-  folders[new_folder_name]=folders[old_folder_name];
+const editName = (folders, old_folder_name, new_folder_name) => {
+  const currentFolder = folders[old_folder_name];
+  folders[new_folder_name] = folders[old_folder_name];
   delete folders[old_folder_name];
   return folders;
-}
-module.exports = {softDeleteFolder, checkIfDeleted,editName,ifExists };
-
+};
+module.exports = { softDeleteFolder, checkIfDeleted, editName, ifExists };
