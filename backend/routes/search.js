24a1aa2b2b38fb092b704ea4c271742ade3f99a4
RavjotSandhu <nand2072@gmail.com>
2022-04-16 14:35:29 +0530

    route change

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const { searchVideoname } = require("../controllers/notes/search");
-router.get("/", searchVideoname);
+router.get("/video", searchVideoname);
 module.exports = router;
