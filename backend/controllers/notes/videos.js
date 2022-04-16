5cb37100878e2b70db45232128dffcd8066924e8
RavjotSandhu <nand2072@gmail.com>
2022-04-16 13:24:58 +0530

    add pagination to videolist

--- backend/controllers/notes/videos.js
+++ backend/controllers/notes/videos.js
@@ -6,14 +6,19 @@ const readallvideos = async (req,res) => {
 
   try {
     const user_id=authByToken(req);
-    const videos = await Video.find({user_id },{ video_id: 1, video_name: 1,video_url:1 });
-    
-    if (!videos) {
+    const page=parseInt(req.query.page);
+    const limit=parseInt(req.query.limit);
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const totalDocuments= await Video.countDocuments({user_id });
+
+    const result = await Video.find({user_id },{ video_id: 1, video_name: 1,video_url:1 }).skip(startIndex).limit(limit);
+
+    if (!result) {
       res.status(404).json({ message: "no video not found" });
     } else {
-      res.status(200).json({ message: "success", videos });
+      res.status(200).json({ message: "success",result });
     }
-    
   } catch (err) {
     console.log(err);
   }
