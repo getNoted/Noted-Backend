5222927104e7855dc360cb8aa0147bf11e6c4321
RavjotSandhu <nand2072@gmail.com>
2022-04-16 13:48:12 +0530

    search vieos by title

---
+++ backend/controllers/notes/search.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const Video = require("../../models/video");
+const { authByToken } = require("../../utils/auth");
+
+const searchVideoname = async (req, res) => {
+    try{
+        const user_id = authByToken(req);
+        const { videoname } = req.query;
+        const videos = await Video.find({user_id, video_name: {$regex: `${videoname}`, $options: "i"}});
+        if(videos){
+            res.status(200).json({message: "fetched relevant videos", videos});
+        }else{
+            res.status(404).json({message: "no video found"});
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+module.exports = { searchVideoname };
