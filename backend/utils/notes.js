ebf14118837ba8019a06342187864788273d8279
Shofiya2003 <shofiyabootwala@gmail.com>
2022-04-17 16:39:48 +0530

    modified the formattimestamp function in utils

--- backend/utils/notes.js
+++ backend/utils/notes.js
@@ -3,6 +3,14 @@ const formattimestamp = (timestamp) => {
     while (timearr.length !== 3) {
       timearr.unshift("00");
     }
+    timearr.forEach((element,index) => {
+      if(element.length!=2){
+        element='0'+element;
+        
+        timearr[index]=element
+      }
+    });
+   
     let formattedtimestamp = "";
     timearr.forEach((element, index) => {
       if (index === 2) formattedtimestamp += element;
