59896fdaced9d8e5f715bb5640a821e6f1b30e55
Shofiya2003 <shofiyabootwala@gmail.com>
2022-04-21 17:00:45 +0530

    added the getVideos of a folder api

--- backend/routes/folders.js
+++ backend/routes/folders.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { createfolder,getFolders,deleteFolder, editFoldername } = require("../controllers/folders/folder");
+const { createfolder,getFolders,deleteFolder, editFoldername,getVideos } = require("../controllers/folders/folder");
 router.post("/delete", deleteFolder);
 router.post("/create", createfolder);
 router.get("/getfolders", getFolders);
 router.post("/editname",editFoldername);
+router.post("/getvideos",getVideos);
 module.exports = router;
