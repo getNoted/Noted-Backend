cfe4b799a54c75018fe2569679e91ad3395f01bd
Shofiya2003 <shofiyabootwala@gmail.com>
2022-04-28 22:27:54 +0530

    sorted the timestamp

--- backend/controllers/notes/videos.js
+++ backend/controllers/notes/videos.js
@@ -13,8 +13,8 @@ const readallvideos = async (req,res) => {
     const endIndex = page * limit
     const totalDocuments= await Video.countDocuments({user_id });
 
-    const result = await Video.find({user_id,is_deleted:deleted==='true' },{ video_id: 1, video_name: 1,video_url:1 }).skip(startIndex).limit(limit);
-
+    const result = await Video.find({user_id,is_deleted:deleted==='true' },{ video_id: 1, video_name: 1,video_url:1,created_at:1 }).sort({created_at:-1}).skip(startIndex).limit(limit);
+    console.log(result);
     if (!result) {
       res.status(404).json({ message: "no video not found" });
     } else {
